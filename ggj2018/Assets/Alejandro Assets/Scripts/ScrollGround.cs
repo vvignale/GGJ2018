82cb02485391bb7bbfe979749d7356360175d9a6
Alejandro Quan-Madrid <quanmadrid@gmail.com>
2018-01-27 19:31:21 -0800

    updated main game layout

---
+++ ggj2018/Assets/Alejandro Assets/Scripts/ScrollGround.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollGround : MonoBehaviour {
+    public float scrollSpeedx = 0.5F;
+	public float scrollSpeedy = 0.5F;
+
+    //don't forget to change the image to a texture and set the boundary to repeat, not clamp
+
+    public Renderer rend;
+    void Start() {
+        rend = GetComponent<Renderer>();
+    }
+    void Update() {
+        float offsetX = Time.time * scrollSpeedx;
+		float offsetY = Time.time * scrollSpeedy;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+    }
+}
---
+++ ggj2018/Assets/Alejandro Assets/Scripts/escQuit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class escQuit : MonoBehaviour 
+
+{
+    void Update () 
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+        
+    }
+}
